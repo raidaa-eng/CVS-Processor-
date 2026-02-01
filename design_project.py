3f3984144d9561260e121b407f7c5c6f40b4f87c
Raida Alam <raidaalam7@gmail.com>
2026-01-31 20:35:16 -0700

    Update design_project.py

--- design_project.py
+++ design_project.py
@@ -1,10 +1,4 @@
 # design_project.py
-# ENDG 233 F24
-# STUDENT NAME(S) Raida Alam and Shaimaa Azad
-# GROUP NAME 20
-# A terminal-based data analysis and visualization program in Python.
-# You must follow the specifications provided in the project description.
-# Remember to include docstrings and comments throughout your code.
 
 import numpy as np
 import math
